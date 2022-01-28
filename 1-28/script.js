fbcdff0cd4392929c955880d430f8479d58c80ca
Mark Betnel <itmeson@gmail.com>
2022-01-28 10:54:17 -0800

    Built first row of guessgrid, added keyboard input to fill in spaces as typed, backspace to delete, and enter to check the current word. New things for students include the string template substitution, the loop to build first row, and the use of different keydown event types.

--- 1-28/script.js
+++ 1-28/script.js
@@ -1,4 +1,5 @@
 var todaysWord = pickRandomWord();
+var letterIndex = 0;
 
 
 function pickRandomWord() {
@@ -12,14 +13,15 @@ row.setAttribute("id", rowname);
 row.setAttribute("class", "row");
 document.getElementById("guessgrid").appendChild(row);
 
-
-let input = document.createElement("div");
-input.setAttribute("type", "text");
-input.setAttribute("id", `00`);
-input.setAttribute("class", "letterbox");
-input.setAttribute("maxlength", 1);
-input.setAttribute("size", 1);
-document.getElementById(rowname).appendChild(input);
+for (let i = 0; i < 5;  i++) {
+  let input = document.createElement("div");
+  input.setAttribute("type", "text");
+  input.setAttribute("id", `0${i}`);
+  input.setAttribute("class", "letterbox");
+  input.setAttribute("maxlength", 1);
+  input.setAttribute("size", 1);
+  document.getElementById(rowname).appendChild(input);
+}
 
 document.addEventListener('keydown', keyDown);
 
@@ -27,6 +29,27 @@ function keyDown(e) {
   let key = e.key;
   console.log(key)
   if (key.length === 1 && key.match(/[a-z]/i)) {
-    document.getElementById('00').innerHTML = key;
+    document.getElementById(`0${letterIndex}`).innerHTML = key;
+    letterIndex = letterIndex + 1;
+  }
+  else if (key === "Backspace" && letterIndex > 0) {
+    letterIndex = letterIndex - 1;
+    document.getElementById(`0${letterIndex}`).innerHTML = "";
+  }
+  else if (key === "Enter" && letterIndex === 5) {
+    checkWord();
+  }
+}
+
+function checkWord() {
+  let guess = "";
+  for (let i = 0; i < 5; i++) {
+    guess = guess + document.getElementById(`0${i}`).innerHTML;
+  }
+  if (guess === todaysWord) {
+    alert("You win!");
+  }
+  else {
+    alert("You lose!");
   }
 }
